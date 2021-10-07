00c90023e9337d9caa53042fb9070675ff6739b4
Roy Rasugu <roy.rasugu@student.moringaschool.com>
2021-10-07 12:28:32 +0300

    Initial Commit

---
+++ gallerio/apps.py
@@ -0,0 +1,5 @@
+from django.apps import AppConfig
+
+
+class GallerioConfig(AppConfig):
+    name = 'gallerio'
---
+++ virtual/bin/django-admin.py
@@ -0,0 +1,5 @@
+#!/home/royrasugu/Documents/moringa core projects/gallery/virtual/bin/python3.8
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
