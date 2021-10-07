1aa88891e2cc42ade1b4e6fe6178b7c0dd819723
Roy Rasugu <roy.rasugu@student.moringaschool.com>
2021-10-07 13:19:42 +0300

    use the render import instead of httpresponse

--- gallerio/views.py
+++ gallerio/views.py
@@ -1,6 +1,5 @@
 from django.shortcuts import render
-from django.http import HttpResponse
 
 # Create your views here.
 def welcome(request):
-    return HttpResponse('Welcome to Mygallery')
+    return render(request,'homepage.html')
