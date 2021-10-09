e29357f2b9265a8a2874a784ca5b072159d3cef4
Roy Rasugu <roy.rasugu@student.moringaschool.com>
2021-10-09 08:16:56 +0300

    add location and category models

--- gallerio/models.py
+++ gallerio/models.py
@@ -5,3 +5,19 @@ class Image(models.Model):
     name = models.CharField(max_length = 30)
     description = models.TextField()
     # image_image = models.ImageField(upload_to = 'images/', default = None)
+
+    def __str__(self):
+        return self.name
+
+class Category(models.Model):
+    name = models.CharField(max_length = 30)
+
+    def __str__(self):
+        return self.name
+
+class Location(models.Model):
+    name = models.CharField(max_length = 30)
+
+    def __str__(self):
+        return self.name
+
