0ca47c68d2a97f7e4adc318f6c82253e5e666a47
Roy Rasugu <roy.rasugu@student.moringaschool.com>
2021-10-09 09:56:39 +0300

    implement the test method for save

--- gallerio/models.py
+++ gallerio/models.py
@@ -9,11 +9,15 @@ class Image(models.Model):
     def __str__(self):
         return self.name
 
+    def save_image(self):
+        self.save()
+        
 class Category(models.Model):
     name = models.CharField(max_length = 30)
 
     def __str__(self):
         return self.name
+
 
 class Location(models.Model):
     name = models.CharField(max_length = 30)
