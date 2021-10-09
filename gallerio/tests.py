d297ae12c0860a792e77b669aa5f3ef7f0df182e
Roy Rasugu <roy.rasugu@student.moringaschool.com>
2021-10-09 08:19:47 +0300

    run the first test instance

--- gallerio/tests.py
+++ gallerio/tests.py
@@ -1,3 +1,19 @@
 from django.test import TestCase
+from .models import Image
 
 # Create your tests here.
+class ImageTestClass(TestCase):
+
+    #set up method
+    def setUp(self):
+        self.cat = Image(name = 'cat', description = 'This cat was made in china')
+
+    # Testing instance
+    def test_instace(self):
+        self.assertTrue(isinstance(self.cat,Image))
+
+    # # Testing Save Method
+    # def test_save_method(self):
+    #     self.cat.save_image()
+    #     images = Image.objects.all()
+    #     self.assertTrue(len(images) > 0)
