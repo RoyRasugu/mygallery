bd3266917b0106436e81f454814281f36f357cb7
Roy Rasugu <roy.rasugu@student.moringaschool.com>
2021-10-09 08:02:25 +0300

    create the image model

--- gallerio/models.py
+++ gallerio/models.py
@@ -1,3 +1,7 @@
 from django.db import models
 
 # Create your models here.
+class Image(models.Model):
+    name = models.CharField(max_length = 30)
+    description = models.TextField()
+    # image_image = models.ImageField(upload_to = 'images/', default = None)
