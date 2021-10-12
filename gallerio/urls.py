f3c660f532dca8251da8c89d76ba5802f0d0e47d
Roy Rasugu <roy.rasugu@student.moringaschool.com>
2021-10-12 07:27:28 +0300

    refine the urls

--- gallerio/urls.py
+++ gallerio/urls.py
@@ -1,6 +1,14 @@
+from django.conf import settings
 from django.conf.urls import url
+from django.conf.urls.static import static
+
+from tribune.settings import MEDIA_ROOT
 from . import views
 
 urlpatterns = [
-    url('^$',views.welcome,name = 'welcome'),
+    url('^$',views.gallery,name = 'gallery'),
+    url(r'^search/', views.search_results, name = 'search_results'),
 ]
+if settings.DEBUG:
+    urlpatterns+= static(settings.MEDIA_URL, document_root = MEDIA_ROOT)
+    
